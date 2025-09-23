8fc7d851dac7f426f9b055dfa7f99af00411c440
Hirotaka-Tambo <tankhdigriotvan@gmail.com>
2025-09-23 13:38:50 +0900

    feat:: ソート機能の追加/css修正

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,14 +1,22 @@
 import "./style.css";
 
 type Props = {
+    // 全て/進行中/完了
     filter: "All" | "Todo" | "Done";
     setFilter: (filter: "All" | "Todo" | "Done") => void;
+    
+    // ソート順
+    sortKey: "priority"|"tag"|"createdAt";
+    setSortKey: (sort: "priority"|"tag"|"createdAt") => void;
 };
 
-const Filter = ({ filter, setFilter}: Props) => {
+const Filter = ({ filter, setFilter, sortKey, setSortKey}: Props) => {
     return (
     <div>
     <hr></hr>
+    <div className="filter-group">
+
+    {/*フィルター(All/Todo/Done)の表示*/ }
     <div className="filter-container">
         <label>
         <input
@@ -35,6 +43,40 @@ const Filter = ({ filter, setFilter}: Props) => {
         Done
         </label>
     </div>
+
+    {/*ソートの表示 */}
+    <div className="sort-container">
+        <label>
+            <input
+            type="radio"
+            name="sort"
+            checked={sortKey === "createdAt"}
+            onChange={() => setSortKey("createdAt")}
+            />
+            作成日時順
+        </label>
+        <label>
+            <input
+            type="radio"
+            name="sort"
+            checked={sortKey === "priority"}
+            onChange={() => setSortKey("priority")}
+            />
+            優先度順
+        </label>
+        <label>
+            <input
+            type="radio"
+            name="sort"
+            checked={sortKey === "tag"}
+            onChange={() => setSortKey("tag")}
+            />
+            タグ順
+        </label>
+    </div>
+
+    </div>
+
 
     
 </div>
